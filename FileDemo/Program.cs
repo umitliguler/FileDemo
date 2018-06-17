0a6431d286f220ee4bd0206e1ae124e9c04a630d
umitliguler <info.umitguler@gmail.com>
2018-06-17 11:56:56 +0300

    first

---
+++ FileDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = @"c:\vidobu\cs_file.txt";
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            var loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+            File.WriteAllText(filePath, loremText + Environment.NewLine);
+
+            var strArr = new[] { "Ocak", "Şubat", "Mart" };
+
+            //File.WriteAllLines(filePath, strArr);
+            File.AppendAllLines(filePath, strArr);
+            
+        }
+    }
+}
